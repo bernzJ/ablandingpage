0d630d4688410fc1413b769a06d081b9b88f6cd1
Ben J <ben-0_0-@hotmail.com>
2019-10-14 21:21:15 -0400

    Updates

---
+++ precache-manifest.e77ee004a117981405f9954464e99d40.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a2aa2c8e6524112141cc14532fa72a7",
+    "url": "/bernzJ/ablandingpage.git/index.html"
+  },
+  {
+    "revision": "2ddfc9de55fdbd850207",
+    "url": "/bernzJ/ablandingpage.git/static/css/2.76163712.chunk.css"
+  },
+  {
+    "revision": "1af80c5cae72d6507c19",
+    "url": "/bernzJ/ablandingpage.git/static/css/main.2b068952.chunk.css"
+  },
+  {
+    "revision": "2ddfc9de55fdbd850207",
+    "url": "/bernzJ/ablandingpage.git/static/js/2.f2065476.chunk.js"
+  },
+  {
+    "revision": "a31d8325a11de07b7544",
+    "url": "/bernzJ/ablandingpage.git/static/js/3.6bba8d33.chunk.js"
+  },
+  {
+    "revision": "0863f58f93430d0b6856",
+    "url": "/bernzJ/ablandingpage.git/static/js/4.1de33dbc.chunk.js"
+  },
+  {
+    "revision": "02a4b1c51740c66617ce",
+    "url": "/bernzJ/ablandingpage.git/static/js/5.56974062.chunk.js"
+  },
+  {
+    "revision": "5b1cebdb0bb0c64e3fc5",
+    "url": "/bernzJ/ablandingpage.git/static/js/6.b3412d10.chunk.js"
+  },
+  {
+    "revision": "e2e0ef34483f95a443d3",
+    "url": "/bernzJ/ablandingpage.git/static/js/7.4d1c0c18.chunk.js"
+  },
+  {
+    "revision": "1af80c5cae72d6507c19",
+    "url": "/bernzJ/ablandingpage.git/static/js/main.828bdb73.chunk.js"
+  },
+  {
+    "revision": "f7ccb753cbfce12ac894",
+    "url": "/bernzJ/ablandingpage.git/static/js/runtime-main.25d3b83a.js"
+  },
+  {
+    "revision": "e697b395b80e04c272c67bdb13dec326",
+    "url": "/bernzJ/ablandingpage.git/static/media/1.e697b395.png"
+  },
+  {
+    "revision": "657c5f88a709cd94c06b524b880ac18a",
+    "url": "/bernzJ/ablandingpage.git/static/media/2.657c5f88.png"
+  },
+  {
+    "revision": "23e07df1e6f92de05ca20b7a21db7282",
+    "url": "/bernzJ/ablandingpage.git/static/media/3.23e07df1.png"
+  },
+  {
+    "revision": "a74649ddeb5da1e04668b10d0e558d49",
+    "url": "/bernzJ/ablandingpage.git/static/media/Logo.a74649dd.png"
+  },
+  {
+    "revision": "5a3dff328b26dc701683d3ccbc3b09a8",
+    "url": "/bernzJ/ablandingpage.git/static/media/commercial.5a3dff32.png"
+  },
+  {
+    "revision": "a439784355e786ddf2f20e98b23c38cb",
+    "url": "/bernzJ/ablandingpage.git/static/media/competitif.a4397843.png"
+  },
+  {
+    "revision": "4fce06c2adcdd7920ad627445959c6ce",
+    "url": "/bernzJ/ablandingpage.git/static/media/parallax.4fce06c2.jpg"
+  },
+  {
+    "revision": "9b0944f37a5370fe1583efd88ee7f7cc",
+    "url": "/bernzJ/ablandingpage.git/static/media/residential.9b0944f3.png"
+  },
+  {
+    "revision": "b50746f559bef30f30ca3e4587308398",
+    "url": "/bernzJ/ablandingpage.git/static/media/snowblower.b50746f5.png"
+  }
+]);
